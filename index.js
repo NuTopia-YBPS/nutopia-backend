e21b58fa8201b058b0659806c8f966dd7957dcc5
RishiMenon2004 <rishimenon82004@gmail.com>
2022-06-27 21:12:05 +0530

    added login api path added signup api path wip register api path

--- index.js
+++ index.js
@@ -1,46 +1,114 @@
-import express, { query } from "express";
+import express from "express";
 import _ from "lodash";
 import fs from "fs-extra";
+import bp from "body-parser";
 import cors from "cors";
 import "dotenv/config";
 import helmet from "helmet";
-import bodyParser from "body-parser";
 import { initializeApp } from "firebase/app";
-import { DocumentData, getDocs, query, QueryDocumentSnapshot, where, collection, getFirestore } from "firebase/firestore";
+import nodemailer from "nodemailer";
+import { renderString } from "nunjucks";
+import { scryptSync, randomBytes, timingSafeEqual } from "crypto";
+
+import {
+  getFirestore,
+  collection,
+  doc,
+  where,
+  setDoc,
+  getDocs,
+  query,
+} from "firebase/firestore";
+
 const app = initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+});
+
+const htmlBody = fs.readFileSync("mail.html", "utf8");
+
+const Mail = nodemailer.createTransport({
+  host: "info@nutopia.in",
+  port: 587,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: process.env.MAILER_EMAIL, // generated ethereal user
+    pass: process.env.MAILER_PASS, // generated ethereal password
+  },
 });
 
 const firestore = getFirestore(app);
 
 const server = express()
-  .use(cors({ origin: "*" }))
+  .use(
+    cors({
+      origin: "*",
+    })
+  )
   .use(helmet())
-  .use(express.text());
+  .use(bp.json());
 
 server.get("/", async (req, res) => {
-  res.status(200).json({ data: "Hello World" });
-});
-server.post("/register", async (req, res) => {
-  let success = false;
-  // Querying
-  const { schoolId, events } = typeof req.body === "string" ? JSON.parse(req?.body) : req.body;
-
-  const schoolQuery = query(collection(firestore, "registrations"), where("schoolId", "==", schoolId));
-  const matchedData = await getDocs(schoolQuery);
-  // Check Query
-  matchedData.forEach(async (data, index) => {
-    console.log(data);
+  res.status(200).json({
+    data: "Hello World",
   });
+});
+
+server.post("/signup", async (req, res) => {
+  const { schoolId, password } =
+    typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+
+  let success = true;
+
+  if (!req.body)
+    return res.status(400).json({ success: false, message: "No body" });
+
+  const salt = randomBytes(16).toString("hex");
+  const hashedPass = scryptSync(password, salt, 64).toString("hex");
+  const obj = { schoolId, password: `${salt}:${hashedPass}` };
+
+  // Database code here
+  const accountsCollection = collection(firestore, "school_login_accounts");
+  await setDoc(doc(accountsCollection, schoolId), obj).catch(
+    (e) => (success = false)
+  );
 
   // Send the result
-  res.status(200).json({ success });
+  res.status(200).json({ success, schoolId, password });
+});
+
+server.post("/login", async (req, res) => {
+  if (!req.body)
+    return res.status(400).json({ success: false, message: "No body" });
+
+  const { schoolId, password } =
+    typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+
+  const accountsCollection = collection(firestore, "school_login_accounts");
+
+  const snapshot = await getDocs(
+    query(accountsCollection, where("schoolId", "==", schoolId))
+  );
+
+  if (snapshot.length === 0) return res.status(200).json({ success: false });
+
+  const doc = snapshot.docs[0];
+  const [salt, key] = doc.get("password").split(":");
+
+  const hashedBuffer = scryptSync(password, salt, 64);
+  const keyBuffer = Buffer.from(key, "hex");
+
+  const match = timingSafeEqual(hashedBuffer, keyBuffer);
+
+  return res.status(200).json({ success: Boolean(match) });
+});
+server.post("/register", async (req, res) => {
+  Mail.sendMail({html: renderString(htmlBody, {name: "John Doe"})});
 });
 server.listen(4000, () => {
   console.log("Server running on http://localhost:4000");
