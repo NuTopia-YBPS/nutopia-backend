3e6939a155e644a4a1990b1b93fc869e5ee48b60
R-Rajaneesh <rajaneeshr2006@gmail.com>
2022-07-05 14:37:17 +0530

    added local json db

--- test.js
+++ test.js
@@ -10,8 +10,8 @@ data.forEach((school, i) => {
   school.name.split(" ").forEach((e) => (schoolshort += e[0]));
   if(schoolshort === "CPS" && !e) schoolshort = "CGPS"
 
-  // schoolshort += "#";
-  // schoolshort += nanoid(4);
+  schoolshort += "#";
+  schoolshort += nanoid(4);
   if (!d.includes(schoolshort)) d.push(schoolshort);
   else throw new Error(`${school.name}-${schoolshort}`);
   // fetch("http://localhost:4000");
