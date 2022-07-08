7c97a8188bc28a9ad6172f3bcb4183e91515c0d6
R-Rajaneesh <rajaneeshr2006@gmail.com>
2022-07-08 14:51:26 +0530

    FINISHED!!!!!!!!!!!
    Co-authored-by: Turbo Defender <RishiMenon2004@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 import bp from "body-parser";
 import cors from "cors";
-import {
-  randomBytes,
-  scryptSync,
-  timingSafeEqual
-} from "crypto";
+import { randomBytes, scryptSync, timingSafeEqual } from "crypto";
 import "dotenv/config";
 import express from "express";
-import {
-  initializeApp
-} from "firebase/app";
-import {
-  collection,
-  doc,
-  getDocs,
-  getFirestore,
-  query,
-  setDoc,
-  updateDoc,
-  where
-} from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { collection, doc, getDocs, addDoc, getFirestore, query, setDoc, updateDoc, where, deleteDoc, deleteField } from "firebase/firestore";
 import fs from "fs-extra";
 import helmet from "helmet";
 import _ from "lodash";
@@ -55,14 +40,7 @@ const Mail = nodemailer.createTransport({
 
 const firestore = getFirestore(app);
 
-const server = express()
-  .use(
-    cors({
-      origin: "*"
-    }),
-  )
-  .use(helmet())
-  .use(bp.json());
+const server = express().use(cors()).use(helmet()).use(bp.json());
 
 server.get("/", async (req, res) => {
   res.status(200).json({
@@ -71,18 +49,14 @@ server.get("/", async (req, res) => {
 });
 
 server.post("/signup", async (req, res) => {
-  const {
-    schoolId,
-    password,
-    email,
-    schoolName
-  } = req.body;
+  const { schoolId, password, email, schoolName } = req.body;
   let success = true;
 
-  if (!req.body) return res.status(400).json({
-    success: false,
-    message: "No body"
-  });
+  if (!req.body)
+    return res.status(400).json({
+      success: false,
+      message: "No body",
+    });
 
   const salt = randomBytes(16).toString("hex");
   const hashedPass = scryptSync(password, salt, 64).toString("hex");
@@ -90,43 +64,50 @@ server.post("/signup", async (req, res) => {
     schoolId,
     password: `${salt}:${hashedPass}`,
     email,
-    schoolName
+    schoolName,
   };
 
   // Database code here
   const accountsCollection = collection(firestore, "school_login_accounts");
   await setDoc(doc(accountsCollection, schoolId), obj).catch((e) => (success = false));
-  db.createData("school_logins", `${schoolId}`, _.defaultsDeep(obj, {
-    realPassword: password
-  }));
+  db.createData(
+    "school_logins",
+    `${schoolId}`,
+    _.defaultsDeep(obj, {
+      realPassword: password,
+    }),
+  );
   // Send the result
   res.status(200).json({
     success,
     schoolId,
     password,
-    email
+    email,
+    message: "Successfully signed up",
   });
 });
 
 server.post("/login", async (req, res) => {
-  if (!req.body) return res.status(400).json({
-    success: false,
-    message: "No body"
-  });
+  let success = false;
+  let message = "Unable to login";
+  if (!req.body)
+    return res.status(400).json({
+      success,
+      message: "No body",
+    });
   const {
-    user: {
-      schoolId,
-      password
-    }
+    user: { schoolId, password },
   } = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
 
   const accountsCollection = collection(firestore, "school_login_accounts");
 
   const snapshot = await getDocs(query(accountsCollection, where("schoolId", "==", schoolId)));
 
-  if (snapshot.length === 0) return res.status(200).json({
-    success: false
-  });
+  if (snapshot.length === 0)
+    return res.status(200).json({
+      success,
+      message: "Not found",
+    });
 
   const docu = snapshot.docs[0];
   const [salt, key] = docu.get("password").split(":");
@@ -135,254 +116,209 @@ server.post("/login", async (req, res) => {
   const hashedBuffer = scryptSync(password, salt, 64);
   const keyBuffer = Buffer.from(key, "hex");
 
-  let success = timingSafeEqual(hashedBuffer, keyBuffer);
+  success = timingSafeEqual(hashedBuffer, keyBuffer);
+  let UserToken = userToken.toString("hex");
+  try {
+    if (success) {
+      await updateDoc(doc(accountsCollection, schoolId), {
+        userToken: userToken.toString("hex"),
+      });
+      return res.status(200).json({ message: "Successfully Logged in", success: true, userToken: UserToken });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  return res.status(400).json({ message: "Unable to login", success: false });
+});
 
-  await updateDoc(doc(accountsCollection, schoolId), {
-    userToken: userToken.toString("hex"),
-  }).catch((e) => (success = false));
+server.post("/logout", async (req, res) => {
+  const { userToken } = req.body;
+  // Database code here
+  const accountsCollection = collection(firestore, "school_login_accounts");
+  try {
+    await deleteField(query(accountsCollection, where("userToken", "==", userToken)));
+  } catch {
+    return res.status(400).json({
+      success: false,
+      message: "Couldn't log you out, please try again",
+    });
+  }
 
-  return res.status(200).json({
-    success: Boolean(success),
-    userToken: userToken.toString("hex"),
+  res.status(200).json({
+    success: true,
+    message: "Successfully logged out",
   });
 });
 
 server.post("/mail", async (req, res) => {
+  let success = false;
+  if (!req.body)
+    return res.status(200).json({
+      success,
+      message: "No body",
+    });
   Mail.sendMail({
     from: '"NuTopia" <info@nutopia.in>',
     to: req.body.userEmail,
     cc: [process.env.MAILER_USER],
-    html: nunjucks.renderString(htmlBody, {
-      schoolName: "Yuvabharathi Public School",
-      events: [{
-          name: "<insert event name here>",
-          isTeam: true,
-          teams: [{
-              teamName: "<insert team name here>",
-              members: [{
-                  name: "<insert team member name here>",
-                  class: "<insert team member class here>",
-                },
-                {
-                  name: "<insert team member name here> 1",
-                  class: "<insert team member class here> 1",
-                },
-              ],
-            },
-            {
-              teamName: "<insert team name here> 1",
-              members: [{
-                  name: "<insert team member name here>",
-                  class: "<insert team member class here>",
-                },
-                {
-                  name: "<insert team member name here> 1",
-                  class: "<insert team member class here> 1",
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: "<insert event name here> 1",
-          isTeam: false,
-          participants: [{
-              name: "<insert team member name here>",
-              class: "<insert team member class here>",
-            },
-            {
-              name: "<insert team member name here> 1",
-              class: "<insert team member class here> 1",
-            },
-          ],
-        },
-      ],
-    }),
+    html: nunjucks.renderString(htmlBody, req.body.formData),
+  })
+    .then(() => (success = true))
+    .catch((e) => {
+      success = false;
+    });
+  res.status(200).json({
+    success,
+    message: `Successfully sent mail to ${req.body.userEmail}`,
   });
 });
 
-server.post("/userData", async (req, res) => {
-  if (!req.body) return res.status(400).json({
-    success: false,
-    message: "No body"
-  });
-
+server.post("/register", async (req, res) => {
   const {
-    schoolId,
-    password
+    data: { event, participants, teams, platform },
+    userToken,
   } = req.body;
-
-  const accountsCollection = collection(firestore, "registrations_season2");
-  const snapshot = await getDocs(query(accountsCollection, where("schoolId", "==", schoolId)));
-
-  console.log(snapshot);
-  return;
-  if (snapshot.length === 0) return res.status(200).json({
-    success: false
-  });
-
-  const docu = snapshot.docs[0];
-  docu.get("")
-  const [salt, key] = docu.get("password").split(":");
-  const userToken = scryptSync(schoolId, salt, 16);
-  const hashedBuffer = scryptSync(password, salt, 64);
-  const keyBuffer = Buffer.from(key, "hex");
-
-  const match = timingSafeEqual(hashedBuffer, keyBuffer);
-
-  return res.status(200).json({
-    success: Boolean(match),
-    userToken
-  });
-});
-
-server.post("/validate", async (req, res) => {
-  const {
+  const data = {
     event,
     participants,
     teams,
     platform,
-    userToken
-  } = req.body;
+  };
   let success = true;
-  let message = ""
-
-  if (!req.body) return res.status(400).json({
-    success: false,
-    message: "No body"
-  });
+  let message = "Successfully registered";
+  if (!req.body)
+    return res.status(400).json({
+      success: false,
+      message: "No body",
+    });
 
   // Database code here
   const accountsCollection = collection(firestore, "school_login_accounts");
-  const userTokenQuery = query(accountsCollection, where("userToken", "==", userToken))
+  const userTokenQuery = query(accountsCollection, where("userToken", "==", userToken));
   const userDocumentSnapShot = await getDocs(userTokenQuery);
   const userDocuments = [];
 
-  userDocumentSnapShot.forEach(doc => {
+  userDocumentSnapShot.forEach((doc) => {
     userDocuments.push(doc);
-    console.log(doc.get("schoolId"));
-  })
-
+  });
   const schoolId = userDocuments[0].get("schoolId");
 
   // console.log(schoolId);
 
   const registrationsCollection = collection(firestore, "registrations_season_2");
   const registrationsSnapshot = await getDocs(registrationsCollection, where("schoolId", "==", schoolId));
-  const registrations = []
-  registrationsSnapshot.forEach(doc => {
+  const registrations = [];
+  registrationsSnapshot.forEach((doc) => {
     registrations.push(doc);
-  })
+  });
 
   if (registrations.length > 0) {
-
-
     const phones = [];
     registrations.forEach((registration) => {
-      const teamsRef = registration.get("teams")
+      const teamsRef = registration.get("teams");
 
       if (teamsRef !== undefined) {
         teamsRef.forEach((teamRef) => {
           const membersRef = teamRef["participants"];
           membersRef.forEach((memberRef) => {
-            phones.push(memberRef["phone"])
-          })
-        })
+            phones.push(memberRef["phone"]);
+          });
+        });
       } else {
         const participants = registration.get("participants");
-        console.log(participants)
         participants.forEach((participant) => {
-          phones.push(participant["phone"])
-        })
+          phones.push(participant["phone"]);
+        });
       }
-    })
+    });
 
-    let formPhones = []
-    let formNames = []
+    // let formPhones = []
+    // let formNames = []
 
     if (teams) {
-      teams.forEach(team => {
+      teams.forEach((team) => {
         team.participants.forEach((participant, index) => {
+          let formPhones = [];
+          let formNames = [];
 
-          let formPhones = []
-          let formNames = []
-
-          teams.forEach(team => {
+          teams.forEach((team) => {
             team.participants.forEach((participant, innerIndex) => {
               if (index !== innerIndex) {
-                formPhones.push(participant.phone)
-                formNames.push(participant.name)
+                formPhones.push(participant.phone);
+                formNames.push(participant.name);
               }
-            })
-          })
+            });
+          });
 
           if (phones.includes(participant.phone)) {
             success = false;
-            message = `Phone number already in use (Participant ${index+1} : ${participant.phone})`
+            message = `Phone number already in use (Participant ${index + 1} : ${participant.phone})`;
           } else if (formPhones.includes(participant.phone)) {
             success = false;
-            message = `Phone numbers cannot be same as other members (Participant ${index+1} : ${participant.phone})`
+            message = `Phone numbers cannot be same as other members (Participant ${index + 1} : ${participant.phone})`;
           }
 
           if (formNames.includes(participant.name)) {
             success = false;
-            message = `Name cannot be same as other members (Participant ${index+1} : ${participant.name})`
+            message = `Name cannot be same as other members (Participant ${index + 1} : ${participant.name})`;
           }
-        })
-      })
+        });
+      });
     } else {
       participants.forEach((participant, index) => {
-
-        let formPhones = []
-        let formNames = []
+        let formPhones = [];
+        let formNames = [];
 
         participants.forEach((participant, innerIndex) => {
           if (index !== innerIndex) {
-            formPhones.push(participant.phone)
-            formNames.push(participant.name)
+            formPhones.push(participant.phone);
+            formNames.push(participant.name);
           }
-          console.log(index !== innerIndex, formNames)
-        })
-
+        });
 
         if (phones.includes(participant.phone)) {
           success = false;
-          message = `Phone number already in use (Participant ${index+1} : ${participant.phone})`
+          message = `Phone number already in use (Participant ${index + 1} : ${participant.phone})`;
         } else if (formPhones.includes(participant.phone)) {
           success = false;
-          message = `Phone numbers cannot be same as participants (Participant ${index+1} : ${participant.phone})`
+          message = `Phone numbers cannot be same as participants (Participant ${index + 1} : ${participant.phone})`;
         }
 
         if (formNames.includes(participant.name)) {
           success = false;
-          message = `Name cannot be same as other participants (Participant ${index+1} : ${participant.name})`
+          message = `Name cannot be same as other participants (Participant ${index + 1} : ${participant.name})`;
         }
-      })
+      });
     }
-
   }
   res.status(200).json({
     schoolId,
     success,
-    message
+    message,
   });
+  if (success) {
+    addDoc(registrationsCollection, {
+      data: data,
+      schoolId: schoolId,
+    });
+  }
 });
-
 server.post("/user", async (req, res) => {
-  const {
-    userToken
-  } = req.body; // Database code here
+  const { userToken } = req.body;
+  // Database code here
   const accountsCollection = collection(firestore, "school_login_accounts");
-  const userTokenQuery = query(accountsCollection, where("userToken", "==", userToken))
+  const userTokenQuery = query(accountsCollection, where("userToken", "==", userToken));
   const userDocumentSnapShot = await getDocs(userTokenQuery);
   const userDocuments = [];
-
   userDocumentSnapShot.forEach((doc) => {
+    console.log(doc);
     userDocuments.push(doc);
-  })
-
-  console.log(userDocuments[0].data())
-})
+  });
+  res.status(200).json({
+    userToken,
+  });
+});
 server.listen(4000, () => {
   console.log("Server running on http://localhost:4000");
 });
+process.on("SIGHUP", (req, res) => {});
